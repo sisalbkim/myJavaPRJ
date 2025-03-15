7be110e3a6cef0dab9b5e4ee450b50e64a353e40
master <sisalbkim@gmail.com>
2025-03-15 11:56:55 +0900

    문자열 예제

---
+++ src/main/java/FirstJava.java
@@ -0,0 +1,13 @@
+public class FirstJava {
+    public static void main(String[] args) {
+
+        String str1 = " 난생처음";
+        String str2 = " 자바 ";
+//        아 주석... ctrl+/로 달수있음.
+//        두줄이상은 Ctrl+Shift+/임.
+//        ??? ctrl shift / 아닌거같은데.? 챗지피티가 날 속임.
+//        print <-- 줄 안넘김  println <---줄 넘김
+        System.out.print(str1);
+        System.out.println(str2);
+    }
+}
