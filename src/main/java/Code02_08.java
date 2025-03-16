f9d1c1d4c405cde353b25b3e381c7b0caf3279f0
master <sisalbkim@gmail.com>
2025-03-16 12:13:42 +0900

    8번 78페이지

--- src/main/java/Code02_08.java
+++ src/main/java/Code02_08.java
@@ -7,6 +7,7 @@ public class Code02_08 {
         System.out.println(str2);
 
         String result = str1 + str2;
+//문자열은 더하기만 가능, 빼기는 불가능. 마찬가지로 곱하기랑 나누기도 불가능
 
         System.out.println(result);
     }
