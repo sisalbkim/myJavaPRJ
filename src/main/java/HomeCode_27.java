fe468267396fc759c3b21cad99ac95c6445f9409
master <sisalbkim@gmail.com>
2025-03-22 22:11:18 +0900

    집 68페이지

---
+++ src/main/java/HomeCode_27.java
@@ -0,0 +1,9 @@
+public class HomeCode_27 {
+    public static void main(String[] args) {
+
+        int a = 10, b;
+        b = a++; // b = a를 집어넣고 다음 줄로 넘어가기전에 a값에 +1을 더함
+        System.out.println("a : " + a);
+        System.out.println("b : " + b);
+    }
+}
