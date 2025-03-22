cd88265d28adb68e8d8ede30b5dc58260e3877a0
master <sisalbkim@gmail.com>
2025-03-22 22:02:00 +0900

    집 64페이지

--- src/main/java/Homecode_24.java
+++ src/main/java/Homecode_24.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class Homecode_24 {
     public static void main(String[] args){
 
-        int n = 20000;
+        Scanner s = new Scanner(System.in);
+        System.out.println("초를 입력하세요 : ");
+        int n = s.nextInt();
         int day = n / (24 * 60 * 60);
         int hour = n / (60 * 60) % 24;
         int minute = (n / 60) % 60;
