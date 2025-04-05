ce3ca3c88284fcd29c593332c73ab2816578b5fa
master <sisalbkim@gmail.com>
2025-04-05 11:02:12 +0900

    교재

---
+++ src/main/java/Homecode37.java
@@ -0,0 +1,14 @@
+public class Homecode37 {
+    public static void main(String[] args) {
+        int a = 20, b = 7, c = 30, d = 2, e = 8;
+        int x = a / b + c * d % e;
+        System.out.println("x = " + x);
+
+        boolean y = a > b + c && d < 8;
+        System.out.println("y = " + y);
+
+        int z = 100;
+        z += a + b - c * d / e;
+        System.out.println("z = " + z);
+    }
+}
---
+++ src/main/java/Homecode38.java
@@ -0,0 +1,12 @@
+public class Homecode38 {
+    public static void main(String[] args) {
+        long x = 10000L;
+        float y;
+        y = x;
+        System.out.println("y : " + y);
+
+        double z;
+        z = x;
+        System.out.println("z : " + z);
+    }
+}
