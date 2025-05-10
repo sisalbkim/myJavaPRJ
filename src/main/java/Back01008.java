acc27eed80790d8e1021627c2c89be7a512ee6c2
master <sisalbkim@gmail.com>
2025-05-10 23:27:31 +0900

    연습

---
+++ src/main/java/Back01008.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Back01008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextInt();
+        double m = sc.nextInt();
+        System.out.println(n/m);
+    }
+}
---
+++ src/main/java/Back10869.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Back10869 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        System.out.println(n+m);
+        System.out.println(n-m);
+        System.out.println(n*m);
+        System.out.println(n/m);
+        System.out.println(n%m);
+    }
+}
