b3e77096e7ce5bc58059788c51506dd2abd1653c
master <sisalbkim@gmail.com>
2025-06-06 23:04:58 +0900

    연습

---
+++ src/main/java/Homecode51.java
@@ -0,0 +1,25 @@
+public class Homecode51 {
+    public static void main(String[] args) {
+
+        int score = 80;
+        char grade;
+
+        switch(score) {
+            case 90:
+                grade = 'A';
+                break;
+            case 80:
+                grade = 'B';
+                break;
+            case 70:
+                grade = 'C';
+                break;
+            default:
+                grade = 'D';
+                break;
+
+
+        }
+        System.out.println("grade : " + grade);
+    }
+}
---
+++ src/main/java/Homecode52.java
@@ -0,0 +1,16 @@
+public class Homecode52 {
+    public static void main(String[] args) {
+
+        int score = 80;
+        switch(score) {
+            case 90 :
+                System.out.println("your score is 90");
+            case 80 :
+                System.out.println("your score is 80");
+            case 70 :
+                System.out.println("your score is 70");
+            default:
+                System.out.println("your score is under 70");
+        }
+    }
+}
