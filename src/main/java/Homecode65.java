0d1bcb577601fbccfaa77ea50225772a8f11b5cb
master <sisalbkim@gmail.com>
2025-06-15 19:12:01 +0900

    연습

---
+++ src/main/java/Homecode65.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Homecode65 {
+
+    public static void main(String[] args) {
+
+        int[] score = {90, 88, 95, 82, 75, 100, 85};
+        System.out.println(Arrays.toString(score));
+    }
+}
