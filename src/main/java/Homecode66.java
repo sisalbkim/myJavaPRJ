a799c09b72fc2c5f83b20965ab8a4189862f281e
master <sisalbkim@gmail.com>
2025-06-20 22:41:02 +0900

    연습

---
+++ src/main/java/Homecode66.java
@@ -0,0 +1,9 @@
+public class Homecode66 {
+    public static void main(String[] args) {
+        int[] score = new int[5];
+        System.out.println(score);
+
+        double[] data = new double[7];
+        System.out.println(data);
+    }
+}
