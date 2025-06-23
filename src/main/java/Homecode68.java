700a9660ff318dc01563dd4c2ce546bebbc87e37
master <sisalbkim@gmail.com>
2025-06-23 23:47:47 +0900

    연습

---
+++ src/main/java/Homecode68.java
@@ -0,0 +1,11 @@
+public class Homecode68 {
+    public static void main(String[] args) {
+        int data1[] = {4,2,9,10,1};
+        int data2[];
+
+        data2 = data1;
+
+        System.out.println("data1:" + data1);
+        System.out.println("data2:" + data2);
+    }
+}
