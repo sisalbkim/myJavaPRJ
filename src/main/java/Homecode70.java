54007a2842451c63e4eeb03610a10e636c8542a0
master <sisalbkim@gmail.com>
2025-06-27 15:28:51 +0900

    연습

---
+++ src/main/java/Homecode70.java
@@ -0,0 +1,19 @@
+public class Homecode70 {
+    public static void main(String[] args) {
+
+
+    int[][] data = {{3, 1, 7, 8},
+            {9, 2, 2, 5},
+            {1, 3, 5, 7}};
+    for(
+    int i = 0;
+    i< 3;i++)
+
+    {
+        for (int j = 0; j < 4; j++) {
+            System.out.print(data[i][j] + " ");
+        }
+        System.out.println();
+    }
+  }
+}
