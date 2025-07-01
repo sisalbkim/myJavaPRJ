e10f5da5ffe64db6e4c771c9f872227014b69341
master <sisalbkim@gmail.com>
2025-07-01 19:13:36 +0900

    연습

---
+++ src/main/java/Homecode74.java
@@ -0,0 +1,8 @@
+public class Homecode74 {
+    public static void main(String[] args) {
+        char alpha[] = {'A', 'B', 'C', 'D', 'E'};
+        for (char x : alpha) {
+            System.out.println(x + ":" + (char) (x+32));
+        }
+    }
+}
