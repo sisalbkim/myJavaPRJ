f7080a075dc688801a5a34da3277b3a3ef1951a2
master <sisalbkim@gmail.com>
2025-07-11 00:02:44 +0900

    연습

---
+++ src/main/java/Homecode76.java
@@ -0,0 +1,10 @@
+public class Homecode76 {
+    public static void main(String[] args) {
+
+        String s1 = "hello";
+        String s2 = new String("world");
+
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
