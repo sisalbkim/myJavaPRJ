bbd4167115bb503f6179747ac698e4761a15c0c6
master <sisalbkim@gmail.com>
2025-07-17 00:14:10 +0900

    연습

---
+++ src/main/java/Back02475.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Back02475 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] n = new int[5];
+
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            n[i] = sc.nextInt();
+            sum += n[i]*n[i];
+        }
+
+        System.out.println(sum%10);
+
+    }
+}
