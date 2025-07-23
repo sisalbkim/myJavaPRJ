2a50f80af0f12e2a1ee6a123dfd49416ea7a90d4
master <sisalbkim@gmail.com>
2025-07-24 01:17:48 +0900

    연습

---
+++ src/main/java/Back16394.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Back16394 {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int var = n-1946;
+        System.out.println(var);
+    }
+}
