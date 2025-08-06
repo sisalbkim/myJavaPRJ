f82e7ab3b0456f4064b07950f8c7c9dc2ca1da6e
master <sisalbkim@gmail.com>
2025-08-07 01:39:05 +0900

    연습

---
+++ src/main/java/Back15552.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Back15552 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+
+
+        for (int i = 0; i < T; i++) {
+
+            String[] a = br.readLine().split(" ");
+            int A = Integer.parseInt(a[0]);
+            int B = Integer.parseInt(a[1]);
+            sb.append(A+B).append("\n");
+        }
+
+        System.out.println(sb);
+
+
+    }
+}
