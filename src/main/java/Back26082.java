7266d65a920a5c06b69b920f02b7c19269dc7b09
master <sisalbkim@gmail.com>
2025-08-17 01:56:53 +0900

    연습

---
+++ src/main/java/Back26082.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Back26082 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println((b/a)*c*3);
+    }
+}
