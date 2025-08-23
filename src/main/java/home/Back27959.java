d895146d8e7ad61b826c5ef983249debcfefc1fd
master <sisalbkim@gmail.com>
2025-08-23 17:38:37 +0900

    연습

---
+++ src/main/java/home/Back27959.java
@@ -0,0 +1,21 @@
+package home;
+
+import java.util.Scanner;
+
+public class Back27959 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+
+        if (a*100>=b){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+
+    }
+}
