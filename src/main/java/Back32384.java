7bc5a2cc5674e496ca24c829c0256ee767cdbdc5
master <sisalbkim@gmail.com>
2025-08-30 02:29:12 +0900

    연습

---
+++ src/main/java/Back32384.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Back32384 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int  n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.print("LoveisKoreaUniversity");
+            if (i != n){
+                System.out.print(" ");
+            }
+        }
+    }
+}
