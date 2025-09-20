59119722d4a3a26aa05b5537f5886dcd90531fa6
master <sisalbkim@gmail.com>
2025-09-20 23:02:50 +0900

    연습

---
+++ src/main/java/Back21598.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Back21598 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if (n == 1) {
+            System.out.println("SciComLove");
+        }else if (n == 2) {
+            System.out.println("SciComLove");
+            System.out.println("SciComLove");
+        }
+        sc.close();
+    }
+}
