1d0bcf3e719bf149b4416bcf27cbf15076f12d15
master <sisalbkim@gmail.com>
2025-09-22 23:10:08 +0900

    연습

---
+++ src/main/java/Back32951.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Back32951 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+
+        System.out.println(a-2024);
+    }
+}
