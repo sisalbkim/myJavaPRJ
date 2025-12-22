8c2301e120819e1a0c74142b02bfdc26675ab77d
master <sisalbkim@gmail.com>
2025-12-22 23:34:51 +0900

    연습

---
+++ src/main/java/Back29751.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Back29751 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Double a = sc.nextDouble();
+        Double b = sc.nextDouble();
+
+        double result = (double) (a * b / 2);
+        System.out.printf("%.1f", result);
+
+
+    }
+}
