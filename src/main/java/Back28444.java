d41aed4312043d400f22733f1a4966f24422a438
master <sisalbkim@gmail.com>
2025-12-24 01:00:30 +0900

    연습

---
+++ src/main/java/Back28444.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Back28444 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+
+        int f = (a*b)-(c*d*e);
+        System.out.println(f);
+
+
+    }
+}
