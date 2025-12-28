0ecf9e6329124a3d568537f91ac1818c0c195393
master <sisalbkim@gmail.com>
2025-12-29 00:30:07 +0900

    연습

---
+++ src/main/java/Back24883.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Back24883 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.nextLine();
+
+        if("N".equalsIgnoreCase(a)){
+            System.out.println("Naver D2");
+        }else {
+            System.out.println("Naver Whale");
+        }
+    }
+}
