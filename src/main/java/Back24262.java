caa7cf69df27a09bc796f71aef5299151c7e8630
master <sisalbkim@gmail.com>
2026-01-01 22:53:46 +0900

    연습

---
+++ src/main/java/Back24262.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Back24262 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int m = n/2;
+        System.out.println("1");
+        System.out.println("0");
+    }
+}
