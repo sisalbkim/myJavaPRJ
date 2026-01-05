4ae078ea12af8f55fdf7ca86f65d1667d43dc90b
master <sisalbkim@gmail.com>
2026-01-06 02:25:43 +0900

    연습

---
+++ src/main/java/Back28113.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Back28113 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a<b){
+            System.out.println("Bus");
+        } else if (a>b) {
+            System.out.println("Subway");
+        } else {
+            System.out.println("Anything");
+        }
+    }
+}
