59ec916e55a45ad0b1a8f18314178734cb95bd2b
master <sisalbkim@gmail.com>
2026-01-11 00:44:41 +0900

    연습

---
+++ src/main/java/Back30030.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Back30030 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int b = sc.nextInt();
+
+        System.out.println(b/11*10);
+    }
+}
