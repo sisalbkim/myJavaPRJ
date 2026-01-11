6fc5eed4e3773050abb37405f18c31860b39291d
master <sisalbkim@gmail.com>
2026-01-12 01:11:58 +0900

    연습

---
+++ src/main/java/Back34849.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Back34849 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+
+        if(n*n<=100000000L){
+            System.out.println("Accepted");
+        }else{
+            System.out.println("Time limit exceeded");
+        }
+    }
+}
