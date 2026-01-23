acfda043fa1e50ed599c84532f44977415666fde
master <sisalbkim@gmail.com>
2026-01-23 15:32:45 +0900

    연습

---
+++ src/main/java/Back29699.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Back29699 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        char[] a = "WelcomeToSMUPC".toCharArray();
+
+        n = n-1;
+        n = n%14;
+
+        System.out.println(a[n]);
+
+    }
+}
