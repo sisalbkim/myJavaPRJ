aa6c99de6138031449e29f623dedbdc9769071a3
master <sisalbkim@gmail.com>
2026-01-27 21:28:29 +0900

    연습

---
+++ src/main/java/Back34665.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Back34665 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+
+        if (str1.equals(str2)) {
+            System.out.println("0");
+        }else {
+            System.out.println("1550");
+        }
+
+    }
+}
