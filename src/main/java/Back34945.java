7170ebe4438e408c0d9a76de7617ee7e287a6efb
master <sisalbkim@gmail.com>
2026-02-18 00:55:01 +0900

    연습

---
+++ src/main/java/Back34945.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Back34945 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        if (a<=5){
+            System.out.println("Oh My God!");
+        }else{
+            System.out.println("Success!");
+        }
+    }
+}
