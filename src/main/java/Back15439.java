3308292b12dadc2cfee053ef8dc2522afeefdfc3
master <sisalbkim@gmail.com>
2026-02-20 18:11:38 +0900

    연습

---
+++ src/main/java/Back15439.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Back15439 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+
+        System.out.println(a*(a-1));
+    }
+}
