ab776183299fcc044c616f13a8df0076d7aecb33
master <sisalbkim@gmail.com>
2026-02-21 20:53:38 +0900

    연습

---
+++ src/main/java/Back25238.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Back25238 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a * (100 - b) >= 10000) {
+            System.out.println(0);
+        } else {
+            System.out.println(1);
+        }
+
+
+
+    }
+}
