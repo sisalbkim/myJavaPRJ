10228a62df47986232d036213980d29f88366eb4
master <sisalbkim@gmail.com>
2026-02-26 23:27:57 +0900

    연습

---
+++ src/main/java/Back04470.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Back04470 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        sc.nextLine();
+
+        for (int i = 1; i <= n; i++) {
+
+            String s = sc.nextLine();
+            System.out.println(i+". "+s);
+        }
+
+    }
+}
