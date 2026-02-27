cbe63d8c4d583cef110c96e7ebbd4212cf841a1f
master <sisalbkim@gmail.com>
2026-02-28 00:53:05 +0900

    연습

---
+++ src/main/java/Back11283.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Back11283 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        char a = sc.next().charAt(0);
+        int b = a;
+
+        System.out.println(b-44031);
+    }
+}
