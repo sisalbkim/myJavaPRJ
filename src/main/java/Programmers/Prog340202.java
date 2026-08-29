4630322f538952536344d14c84307519eb18cd58
master <sisalbkim@gmail.com>
2026-08-29 22:36:34 +0900

    연습

---
+++ src/main/java/Programmers/Prog340202.java
@@ -0,0 +1,15 @@
+package Programmers;
+
+public class Prog340202 {
+    public int solution(int storage, int usage, int[] change) {
+        int total_usage = 0;
+        for(int i=0; i<change.length; i++){
+            usage = usage + (usage * change[i] / 100);
+            total_usage += usage;
+            if(total_usage > storage){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
