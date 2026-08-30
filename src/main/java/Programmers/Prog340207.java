88799826eabd7ca22b48df27cc20aae7eb87c6f8
master <sisalbkim@gmail.com>
2026-08-30 13:23:22 +0900

    연습

---
+++ src/main/java/Programmers/Prog340207.java
@@ -0,0 +1,10 @@
+package Programmers;
+
+public class Prog340207 {
+    public static void main(String[] args) {
+            String message = "Let's go!";
+
+            System.out.println("3\n2\n1");
+                    System.out.println(message);
+        }
+}
