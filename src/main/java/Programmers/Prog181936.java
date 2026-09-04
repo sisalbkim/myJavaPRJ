2f5e2af4e545d8c0d74d657ccf1ae5089a800052
김현규 <sisalbkim@gmail.com>
2026-09-04 10:10:11 +0900

    prog

---
+++ src/main/java/Programmers/Prog181936.java
@@ -0,0 +1,11 @@
+package Programmers;
+
+public class Prog181936 {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        if (number%n==0 && number%m==0){
+            answer = 1;
+        }
+        return answer;
+    }
+}
