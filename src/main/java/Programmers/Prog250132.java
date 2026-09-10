d3921081075419a3d4791df910931a8ab170dcd9
김현규 <sisalbkim@gmail.com>
2026-09-10 09:06:05 +0900

    prog

---
+++ src/main/java/Programmers/Prog250132.java
@@ -0,0 +1,15 @@
+package Programmers;
+
+import java.util.Scanner;
+
+public class Prog250132 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int c = sc.nextInt();
+
+        int b_square = c*c - a*a;
+
+        System.out.println(b_square);
+    }
+}
